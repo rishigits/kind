63197eb0ee9c234ffda08a0a70325dfebcea9131
rishigits <rishigits@users.noreply.github.com>
2025-09-09 14:01:11 -0400

    Create build.sh

---
+++ src/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e -o pipefail
+read -ra arr <<< "$@"
+version=${arr[1]}
+trap 0 1 2 ERR
+# Ensure sudo is installed
+apt-get update && apt-get install sudo -y
+bash /tmp/linux-on-ibm-z-scripts/Kind/${version}/build_kind.sh -y
+tar cvfz kind-${version}-linux-s390x.tar.gz kind/bin/kind
+# Create container image - script above will have already created the image
+docker save -o kind-${version}-linux-s390x.container.tar kindest/base:s390x
+exit 0
