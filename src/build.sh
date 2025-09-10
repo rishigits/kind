19d0ae0b23177f83c1228f29040234c509269da5
rishigits <rishigits@users.noreply.github.com>
2025-09-10 12:41:03 -0400

    Update build.sh

--- src/build.sh
+++ src/build.sh
@@ -4,7 +4,7 @@ read -ra arr <<< "$@"
 version=${arr[1]}
 trap 0 1 2 ERR
 # Ensure sudo is installed
-apt-get update && apt-get install sudo -y
+apt-get update && apt-get install sudo jq -y
 bash /tmp/linux-on-ibm-z-scripts/Kind/${version}/build_kind.sh -y
 tar cvfz kind-${version}-linux-s390x.tar.gz kind/bin/kind
 # Create container image - script above will have already created the image
